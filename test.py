913cbbb56d1b345b2701f6945d3887e58fd21e5f
juanthomasw <104194668+juanthomasw@users.noreply.github.com>
2024-10-25 16:13:41 +0700

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 from models.experimental import attempt_load
 from utils.datasets import create_dataloader
 from utils.general import coco80_to_coco91_class, check_dataset, check_file, check_img_size, check_requirements, \
-    box_iou, non_max_suppression, non_max_suppression_obb, scale_coords, scale_polys, xyxy2xywh, xywh2xyxy, set_logging, increment_path, colorstr
+    box_iou, non_max_suppression, non_max_suppression_gaussianobb, scale_coords, scale_polys, xyxy2xywh, xywh2xyxy, set_logging, increment_path, colorstr
 from utils.metrics import ap_per_class, ConfusionMatrix
 from utils.plots import plot_images, output_to_target, plot_study_txt
 from utils.torch_utils import select_device, time_synchronized, TracedModel
@@ -124,7 +124,7 @@ def test(data,
             # targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
             lb = [targets[targets[:, 0] == i, 1:] for i in range(nb)] if save_hybrid else []  # for autolabelling
             t = time_synchronized()
-            out = non_max_suppression_obb(out, conf_thres=conf_thres, iou_thres=iou_thres, labels=lb, multi_label=True)
+            out = non_max_suppression_gaussianobb(out, conf_thres=conf_thres, iou_thres=iou_thres, labels=lb, multi_label=True)
             # [ xywh theta conf cls ]
             
             t1 += time_synchronized() - t
